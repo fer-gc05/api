fe5007f7277bc47575add1046f03246f55ae8cbd
Fernando Luis Gil Correa <122411406+fer-gc05@users.noreply.github.com>
2024-09-10 16:35:46 -0500

    Update database.py

--- config/database.py
+++ config/database.py
@@ -1,11 +1,17 @@
 import pymysql
+import os
 
 DATABASE_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "0522",
-    "database": "pas",
+    "host": os.getenv("DB_HOST"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME"),
 }
 
 def get_connection():
-    return pymysql.connect(**DATABASE_CONFIG)
+    try:
+        connection = pymysql.connect(**DATABASE_CONFIG)
+        return connection
+    except pymysql.MySQLError as e:
+        print(f"Error connecting to MySQL database: {e}")
+        return None
